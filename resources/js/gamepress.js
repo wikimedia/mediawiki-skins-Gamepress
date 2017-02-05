04932a3fc562b375b54ab2f6566b9ac84983388a
codynguyen1116 <samanthanguyen1116@gmail.com>
2017-02-04 20:07:56 -0600

    Remove wordpress CSS/JS/media technical debt in Gamepress
    Bug: T157202
    Change-Id: I426f47daf138fd4d5fc5224bd0505bfebcdb5956

--- resources/js/gamepress.js
+++ resources/js/gamepress.js
@@ -1,73 +1,11 @@
 jQuery( function( $ ) {
 	/**
-	 * Tabs & Accordion
-	 */
-	$( 'ul.tabs' ).tabs( '> .pane', {effect: 'fade'} );
-	$( '.accordion' ).tabs( '.pane', {tabs: 'h4', effect: 'slide'} );
-	$( '.accordion p:empty' ).remove();
-
-	/**
-	 * Adding transparent wmode to YouTube iframe
-	 */
-	$( '.video-embed iframe' ).each( function() {
-		var url = $( this ).attr( 'src' );
-		if ( url.indexOf( '?' ) == -1 ) {
-			$( this ).attr( 'src', url + '?wmode=transparent' );
-		} else {
-			$( this ).attr( 'src', url + '&wmode=transparent' );
-		}
-	} );
-
-	/**
 	 * Scrolling to top of page
 	 */
 	$( '.scrollup' ).on( 'click', function() {
 		$( 'html, body' ).animate( { scrollTop: 0 }, 1000, 'easeOutQuint' );
 		return false;
 	} );
-
-	$( '.archive-content .video-item:odd' ).addClass( 'l' );
-
-	/**
-	 * Notifications
-	 */
-	$( '.close' ).on( 'click', function() {
-		$( this ).parent( '.message' ).fadeOut( 'normal' );
-	} );
-
-	/**
-	 * Sliders
-	 */
-	if ( $( '#slider' ).not( '.thumbs' ).length > 0 ) {
-		$( '#slider' ).not( '.thumbs' ).nivoSlider( {
-			effect: 'random',
-			slices: 15,
-			boxCols: 8,
-			boxRows: 4,
-			animSpeed: 500,
-			pauseTime: 5000,
-			startSlide: 0,
-			directionNav: true,
-			manualAdvance: true,
-			controlNav: true,
-			controlNavThumbs: false,
-			keyboardNav: true,
-			pauseOnHover: true,
-			manualAdvance: false
-		} );
-	}
-	if ( $( '#slider.thumbs' ).length > 0 ) {
-		$( '#slider.thumbs' ).nivoSlider( {
-			effect: 'random',
-			slices: 15,
-			boxCols: 8,
-			boxRows: 4,
-			animSpeed: 500,
-			pauseTime: 5000,
-			startSlide: 0,
-			controlNavThumbs: true
-		} );
-	}
 
 	/**
 	 * Make the top menu usable on touchscreen devices so that it's possible to
@@ -121,20 +59,4 @@ jQuery( function( $ ) {
 			} );
 		} );
 	};
-
-	/**
-	 * Fallback for browsers without content: css attribute
-	 */
-	var ieVersion = 0;
-	var clientPC = navigator.userAgent.toLowerCase(); // Get client info
-	if ( /msie (\d+\.\d+);/.test( clientPC ) ) { // test for MSIE x.x;
-		ieVersion = ( new Number( RegExp.$1 ) ); // capture x.x portion and store as a number
-	}
-
-	if (
-		!Modernizr.generatedcontent ||
-		( /msie (\d+\.\d+);/.test( clientPC ) /* test for MSIE x.x */ && ieVersion == 8 ) ) {
-		$( '#primary-nav ul li:first-child > a, #primary-nav ul ul li:first-child > a' ).before( $( '<span/>' ).addClass( 'before' ) );
-		$( '.img-bevel' ).before( '<span class="before-img-bevel-fix"></span>' );
-	}
 } );
